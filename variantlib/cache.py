cfe36e54d1fd131e6dc63c80136362bb9cf64293
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-03-27 23:50:27 -0400

    [Type Hint] Minor change in `cache.py`

--- variantlib/cache.py
+++ variantlib/cache.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
 from typing import Any
-from typing import Callable
 from typing import Generic
 from typing import TypeVar
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
 
 T = TypeVar("T")
 
