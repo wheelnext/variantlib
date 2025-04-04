60a3ae760f6f1ba93237adf7864e327ee681ded5
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-04-04 16:08:22 -0400

    Adding `autoflake` to clean up unused imports

--- variantlib/__init__.py
+++ variantlib/__init__.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import importlib.metadata
 
-# flake8: noqa: F401
-from variantlib import logger
+from variantlib import logger  # noqa: F401
 
 __version__ = importlib.metadata.version("variantlib")
