cad68613786bd1b49ed7a98187b3c392f6e8c9bf
Michał Górny <mgorny@gentoo.org>
2025-04-11 14:35:02 +0200

    Use regex to process environment markers instead
    Use regex to preprocess "variants" environment markers rather than
    the internal API of packaging.  While at it, add support for negative
    "not in" expressions, and effectively fix support for dealing with
    compound statements.
    
    Rather than attempting to implement complete boolean logic to process
    various conditions, we replace the variant markers with a "true"
    and "false" conditions that pip/uv can handle.

--- tests/test_envspec.py
+++ tests/test_envspec.py
@@ -1,5 +1,7 @@
 import pytest
 
+from variantlib.envspec import FALSE_STR
+from variantlib.envspec import TRUE_STR
 from variantlib.envspec import evaluate_variant_requirements
 from variantlib.errors import InvalidVariantEnvSpecError
 from variantlib.models.variant import VariantDescription
@@ -9,35 +11,41 @@ from variantlib.models.variant import VariantProperty
 @pytest.mark.parametrize(
     ("properties", "expected"),
     [
-        ([VariantProperty("x86_64", "level", "v3")], ["variant-x86-64"]),
-        ([VariantProperty("x86_64", "sse3", "on")], ["variant-x86-64"]),
-        ([VariantProperty("cuda", "runtime", "12.6")], ["variant-cuda"]),
-        ([VariantProperty("cuda", "other", "x")], []),
-        ([VariantProperty("x", "y", "z")], []),
+        ([VariantProperty("x86_64", "level", "v3")], [False, True, False]),
+        ([VariantProperty("x86_64", "sse3", "on")], [False, True, False]),
+        ([VariantProperty("cuda", "runtime", "12.6")], [True, False, True]),
+        ([VariantProperty("cuda", "other", "x")], [False, False, True]),
+        ([VariantProperty("x", "y", "z")], [False, False, False]),
         (
             [
                 VariantProperty("x86_64", "level", "v3"),
                 VariantProperty("cuda", "runtime", "12.6"),
             ],
-            ["variant-cuda", "variant-x86-64"],
+            [True, True, True],
         ),
     ],
 )
 def test_evaluate_variant_requirements(
-    properties: list[VariantProperty], expected: list[str]
+    properties: list[VariantProperty], expected: list[bool]
 ) -> None:
     requirements = [
         "setuptools",
         "typing-extensions; python_version < '3.11'",
         "variant-cuda; 'cuda :: runtime' in variants",
         "variant-x86-64; 'x86_64' in variants",
+        "variant-cuda-extra; 'cuda' in variants and python_version >= '3.11'",
+        "no-cuda; 'cuda' not in variants",
     ]
     variant_desc = VariantDescription(properties)
 
     assert evaluate_variant_requirements(requirements, variant_desc) == [
         "setuptools",
         "typing-extensions; python_version < '3.11'",
-        *expected,
+        f"variant-cuda; {TRUE_STR if expected[0] else FALSE_STR}",
+        f"variant-x86-64; {TRUE_STR if expected[1] else FALSE_STR}",
+        f"variant-cuda-extra; {TRUE_STR if expected[2] else FALSE_STR} "
+        'and python_version >= "3.11"',
+        f"no-cuda; {TRUE_STR if not expected[2] else FALSE_STR}",
     ]
 
 
@@ -46,7 +54,12 @@ def test_evaluate_variant_requirements(
     [
         "variants == 'x'",
         "variants in 'x'",
+        "variants not in 'x'",
         "'x' == variants",
+        "variants!='x'",
+        "variants in'x'",
+        "variants not in'x'",
+        "'x'!=variants",
     ],
 )
 def test_invalid_variant_requirements(env_spec: str) -> None:
--- variantlib/envspec.py
+++ variantlib/envspec.py
@@ -2,15 +2,31 @@
 
 from __future__ import annotations
 
+import re
 from itertools import chain
 
-from packaging.markers import Op
-from packaging.markers import Value
-from packaging.markers import Variable
 from packaging.requirements import Requirement
 from variantlib.errors import InvalidVariantEnvSpecError
 from variantlib.models.variant import VariantDescription
 from variantlib.models.variant import VariantFeature
+
+TRUE_STR = "python_version != '0'"
+FALSE_STR = "python_version == '0'"
+
+VARIANT_EXPR_RE = re.compile(
+    # 'value' in variants
+    r"(?P<quote>[\"'])(?P<value>.*)(?P=quote) \s* (?P<neg>not \s+)? in \s+ variants\b",
+    re.VERBOSE,
+)
+
+VARIANT_ERROR_RES = [
+    # variants OP ...
+    re.compile(r"\bvariants \s* [=!<>]", re.VERBOSE),
+    # variants [not] in ...
+    re.compile(r"\bvariants \s+ (not \s+)? in", re.VERBOSE),
+    # ... OP variants ; where OP != "in"
+    re.compile(r"[=!<>] \s* variants", re.VERBOSE),
+]
 
 
 def evaluate_variant_requirements(
@@ -39,39 +55,26 @@ def evaluate_variant_requirements(
     new_requirements = []
     for req in requirements:
         parsed_req = Requirement(req)
-        marker = parsed_req.marker
-        # TODO: support more complex markers
-        if marker is None or len(marker._markers) != 1:  # noqa: SLF001
-            new_requirements.append(req)
-            continue
 
-        parsed_marker = marker._markers[0]  # noqa: SLF001
-        assert isinstance(parsed_marker[1], Op)
-        # "variants OP ..." is invalid
-        if (
-            isinstance(parsed_marker[0], Variable)
-            and str(parsed_marker[0]) == "variants"
-        ):
-            raise InvalidVariantEnvSpecError(
-                "'variants' marker can only be used in \"'foo' in variants\" "
-                "expressions"
-            )
-        # we are only interested in "... OP variants"
-        if not (
-            isinstance(parsed_marker[2], Variable)
-            and str(parsed_marker[2]) == "variants"
-        ):
-            new_requirements.append(req)
-            continue
-        # only "in" operator is allowed
-        if str(parsed_marker[1]) != "in" or not isinstance(parsed_marker[0], Value):
-            raise InvalidVariantEnvSpecError(
-                "'variants' marker can only be used in \"'foo' in variants\" "
-                "expressions"
-            )
+        def repl(match: re.Match) -> str:
+            if (match.group("neg") is None) == (match.group("value") in all_values):
+                return TRUE_STR
+            return FALSE_STR
 
-        if str(parsed_marker[0]) in all_values:
-            parsed_req.marker = None
-            new_requirements.append(str(parsed_req))
+        if parsed_req.marker is not None:
+            for error_re in VARIANT_ERROR_RES:
+                if error_re.search(str(parsed_req.marker)) is not None:
+                    raise InvalidVariantEnvSpecError(
+                        "'variants' marker can only be used in \"'foo' in variants\" "
+                        "expressions"
+                    )
+
+            new_marker = VARIANT_EXPR_RE.sub(repl, str(parsed_req.marker))
+            # alter the requirement only if we actually changed anything
+            if new_marker != str(parsed_req.marker):
+                parsed_req.marker = new_marker
+                new_requirements.append(str(parsed_req))
+                continue
+        new_requirements.append(req)
 
     return new_requirements
