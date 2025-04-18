13f6ee02cd8488741828709b230d9bcc4e429867
Michał Górny <mgorny@gentoo.org>
2025-04-18 17:33:41 +0200

    Remove namespace_priority_dict from get_variant_hashes_by_priority()
    Remove the `namespace_priority_dict` that is not used by consumers,
    and is not going to be the part of the final API, before starting
    to rewrite the function.

--- variantlib/api.py
+++ variantlib/api.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import logging
-from collections import defaultdict
 from typing import TYPE_CHECKING
 
 from variantlib.combination import filtered_sorted_variants
@@ -39,68 +38,11 @@ __all__ = [
 def get_variant_hashes_by_priority(
     *,
     variants_json: dict,
-    namespace_priority_dict: dict[str, int] | None = None,
 ) -> Generator[str]:
-    provider_cfgs = PluginLoader.get_supported_configs()
+    provider_cfgs = list(PluginLoader.get_supported_configs().values())
 
-    # sorting providers in priority order:
-    if namespace_priority_dict is not None:
-        if (
-            not isinstance(namespace_priority_dict, dict)
-            or not all(isinstance(key, str) for key in namespace_priority_dict)
-            or not all(isinstance(key, int) for key in namespace_priority_dict.values())
-        ):
-            logger.warning(
-                "Invalid `namespace_priority_dict` provided. Should follow "
-                "format: dict[str:int]. Ignoring..."
-            )
-        else:
-            # ----------- Checking if two plugins hold the same priority ----------- #
-            value_to_keys = defaultdict(list)  # temp storage
-
-            # Populate the dictionary with values and their corresponding keys
-            for key, value in namespace_priority_dict.items():
-                value_to_keys[value].append(key)
-
-            # Isolate the duplicate values and their corresponding keys
-            duplicates = {
-                value: keys for value, keys in value_to_keys.items() if len(keys) > 1
-            }
-
-            if duplicates:
-                logger.warning("Duplicate values and their corresponding keys:")
-                for value, keys in duplicates.items():
-                    logger.warning("Value: %s -> Keys: %s", value, keys)
-
-            # ----------- Checking if two plugins hold the same priority ----------- #
-            for namespace in provider_cfgs:
-                if namespace not in namespace_priority_dict:
-                    logger.warning(
-                        "Plugin: %s is not present in the `namespace_priority_dict`. "
-                        "Will be treated as lowest priority.",
-                        namespace,
-                    )
-                    continue
-
-            # ------------------- Sorting the plugins by priority ------------------ #
-            plugins = sorted(
-                provider_cfgs,
-                key=lambda namespace: namespace_priority_dict.get(
-                    namespace, float("inf")
-                ),
-            )
-
-            sorted_provider_cfgs = [provider_cfgs[namespace] for namespace in plugins]
-    else:
-        sorted_provider_cfgs = list(provider_cfgs.values())
-
-    if sorted_provider_cfgs:
-        for vdesc in filtered_sorted_variants(
-            variants_json["variants"], sorted_provider_cfgs
-        ):
-            yield vdesc.hexdigest
-    else:
-        yield from []
+    for vdesc in filtered_sorted_variants(variants_json["variants"], provider_cfgs):
+        yield vdesc.hexdigest
 
 
 def validate_variant(
