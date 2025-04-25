3c640a917a0be5568c77511269a34955cb2e262c
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-04-25 01:23:31 -0400

    [generate_index_json] Adjustments & Providers supporting list

--- variantlib/commands/generate_index_json.py
+++ variantlib/commands/generate_index_json.py
@@ -7,12 +7,13 @@ import json
 import logging
 import pathlib
 import zipfile
+from collections import defaultdict
 
-from variantlib.constants import METADATA_VARIANT_HASH_HEADER
 from variantlib.constants import METADATA_VARIANT_PROPERTY_HEADER
 from variantlib.constants import METADATA_VARIANT_PROVIDER_HEADER
 from variantlib.constants import VARIANTS_JSON_PROVIDER_DATA_KEY
 from variantlib.constants import VARIANTS_JSON_VARIANT_DATA_KEY
+from variantlib.constants import WHEEL_NAME_VALIDATION_REGEX
 from variantlib.errors import ValidationError
 from variantlib.models.variant import VariantDescription
 from variantlib.models.variant import VariantProperty
@@ -45,11 +46,24 @@ def generate_index_json(args: list[str]) -> None:
 
     vprop_parser = email.parser.BytesParser(policy=email.policy.compat32)
     known_variants: dict[str, VariantDescription] = {}
-    known_namespaces = set()
-    providers: dict[str, str] = {}
+    providers: dict[str, set[str]] = defaultdict(set)
 
     for wheel in directory.glob("*.whl"):
+        # Skip non wheel variants
+        if (wheel_info := WHEEL_NAME_VALIDATION_REGEX.fullmatch(wheel.name)) is None:
+            raise TypeError(
+                f"The file is not a valid python wheel filename: `{wheel.name}`"
+            )
+
+        if wheel_info.group("variant_hash") is None:
+            logger.debug(
+                "Filepath: `%(input_file)s` ... is not a wheel variant.",
+                {"input_file": wheel.name},
+            )
+            continue
+
         with zipfile.ZipFile(wheel, "r") as zip_file:
+            # Find the METADATA file
             for name in zip_file.namelist():
                 if name.endswith(".dist-info/METADATA"):
                     with zip_file.open(name) as f:
@@ -60,64 +74,24 @@ def generate_index_json(args: list[str]) -> None:
                 logger.warning("%s: no METADATA file found", wheel)
                 continue
 
-            if (
-                variant_hash := wheel_metadata.get(METADATA_VARIANT_HASH_HEADER)
-            ) is None:
-                logger.debug("%s: not a Wheel Variant. Skipping ...", wheel)
-                continue
-
+            # Only valid Wheel Variants need to be processed
             if (
                 variant_entries := wheel_metadata.get_all(
                     METADATA_VARIANT_PROPERTY_HEADER
                 )
             ) is None:
                 logger.warning(
-                    "%s: Variant-hash present but no Variant property", wheel
+                    "%(wheel)s: This wheel does not declare any `%(key)s`",
+                    {"wheel": wheel, "key": METADATA_VARIANT_PROPERTY_HEADER},
                 )
                 continue
 
-            if (
-                wheel_providers := wheel_metadata.get_all(
-                    METADATA_VARIANT_PROVIDER_HEADER
-                )
-            ) is None:
-                logger.info("%s: no Variant-provider", wheel)
-
-            else:
-                for wheel_provider in wheel_providers:
-                    ns, _, provider = wheel_provider.partition(",")
-                    ns = ns.strip()
-                    provider = provider.strip()
-                    if not ns or not provider:
-                        raise ValueError(
-                            f"{wheel}: Invalid {METADATA_VARIANT_PROVIDER_HEADER}: "
-                            f"{wheel_provider}"
-                        )
-                    if providers.setdefault(ns, provider) != provider:
-                        raise KeyError(
-                            f"Conflicting providers found for namespace {ns}: "
-                            f"{providers[ns]} and {provider}"
-                        )
+            # ============== Variant Properties Processing ================ #
 
             vprops = [VariantProperty.from_str(vprop) for vprop in variant_entries]
-            for vprop in vprops:
-                known_namespaces.add(vprop.namespace)
 
             try:
                 vdesc = VariantDescription(vprops)
-                if vdesc.hexdigest != variant_hash:
-                    logger.error(
-                        "%(wheel)s has been rejected due to a variant hash mismatch: "
-                        "Found: `%(vhash_found)s` != Calculated: `%(vhash_calc)s`. "
-                        "Will be ignored.",
-                        {
-                            "wheel": wheel,
-                            "vhash_found": variant_hash,
-                            "vhash_calc": vdesc.hexdigest,
-                        },
-                    )
-                    continue
-
             except ValidationError:
                 logger.exception(
                     "%(wheel)s has been rejected due to invalid properties. Will be "
@@ -126,10 +100,35 @@ def generate_index_json(args: list[str]) -> None:
                 )
                 continue
 
-            if vdesc.hexdigest not in known_variants:
-                known_variants[variant_hash] = vdesc
+            if (vhash := vdesc.hexdigest) not in known_variants:
+                known_variants[vhash] = vdesc
 
-    with directory.joinpath("variants.json").open("w") as f:
+            # ============== Variant Providers Processing ================ #
+            if not (
+                wheel_providers := wheel_metadata.get_all(
+                    METADATA_VARIANT_PROVIDER_HEADER, []
+                )
+            ):
+                logger.info(
+                    "%(wheel)s: did not declare any `%(key)s`",
+                    {"wheel": wheel, "key": METADATA_VARIANT_PROVIDER_HEADER},
+                )
+
+            for wheel_provider in wheel_providers:
+                ns, _, provider = wheel_provider.partition(", ")
+                ns = ns.strip()
+                provider = provider.strip()
+                if not ns or not provider:
+                    raise ValueError(
+                        f"{wheel}: Invalid {METADATA_VARIANT_PROVIDER_HEADER}: "
+                        f"{wheel_provider}"
+                    )
+                providers[ns].add(provider)
+
+    for key, values in providers.items():
+        providers[key] = sorted(values)
+
+    with (directory / "variants.json").open(mode="w") as f:
         json.dump(
             {
                 VARIANTS_JSON_PROVIDER_DATA_KEY: providers,
