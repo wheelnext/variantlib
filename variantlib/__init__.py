a8da19ad32f1fb2d916c387de6b8ddf61a5677e1
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-04-29 13:54:00 -0400

    Change to `flit-core`

--- variantlib/__init__.py
+++ variantlib/__init__.py
@@ -1,10 +1,4 @@
 from __future__ import annotations
 
-import importlib.metadata
-
 __package_name__ = "variantlib"
-
-try:
-    __version__ = importlib.metadata.version(__package_name__)
-except importlib.metadata.PackageNotFoundError:
-    __version__ = "unknown"
+__version__ = "0.0.1"
