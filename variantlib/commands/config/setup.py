wheelnext/variantlib

commit 2197d01b0c0600494b97b7efdc47193a5acf7aa3

[Command] `variantlib config setup`: visual glitter

variantlib/commands/config/setup.py
@@ -144,6 +144,8 @@ def setup(args: list[str]) -> None:
 
         else:
             supported_configs = PluginLoader.get_supported_configs()
+
+            ui.clear()
             namespace_priorities = ui.update_key(
                 toml_data,
                 "namespace_priorities",
@@ -151,9 +153,14 @@ def setup(args: list[str]) -> None:
                 known_values_required=True,
             )
 
-            if ui.input_bool(
-                "Do you want to adjust feature priorities?", default=False
-            ):
+            message = (
+                "**Expert-Users Only**\n\n"
+                "~ This setting should be left empty & untouched in most cases ~\n\n"
+                "Do you wish to adjust variant-feature priorities?"
+            )
+
+            ui.clear()
+            if ui.input_bool(message, default=False, height=9):
                 known_features = [
                     VariantFeature(namespace, config.name).to_str()
                     for namespace, provider in sorted(
@@ -169,9 +176,14 @@ def setup(args: list[str]) -> None:
                     known_values_required=False,
                 )
 
-            if ui.input_bool(
-                "Do you want to adjust property priorities?", default=False
-            ):
+            message = (
+                "**Expert-Users Only**\n\n"
+                "~ This setting should be left empty & untouched in most cases ~\n\n"
+                "Do you wish to adjust variant-property priorities?"
+            )
+
+            ui.clear()
+            if ui.input_bool(message, default=False, height=9):
                 feature_priorities = [
                     VariantFeature.from_str(x)
                     for x in toml_data.get("feature_priorities", [])
@@ -205,8 +217,8 @@ def setup(args: list[str]) -> None:
             # Always use multiline output for readability.
             toml_data[key].multiline(multiline=True)
 
+        ui.clear()
         sys.stderr.write(
-            "\n"
             "Final configuration:\n"
             "\n"
             "```\n"
@@ -217,7 +229,9 @@ def setup(args: list[str]) -> None:
         )
         if parsed_args.ui != "urwid":
             if not ui.input_bool(
-                "Do you want to save the configuration changes?", default=True
+                "Do you want to save the configuration changes?",
+                default=True,
+                clear_console=False,
             ):
                 sys.stdout.write("Configuration changes discarded\n")
                 return

variantlib/commands/config/setup_interfaces/console_ui.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 import sys
 from itertools import chain
 from typing import TYPE_CHECKING
@@ -55,7 +56,7 @@ class ConsoleUI:
             return False
         return True
 
-    def input_bool(self, prompt: str, default: bool) -> bool:
+    def input_bool(self, prompt: str, default: bool, *args, **kwargs) -> bool:
         full_prompt = f"{prompt} [{'Y/n' if default else 'y/N'}] "
         while True:
             val = input(full_prompt)
@@ -159,3 +160,9 @@ class ConsoleUI:
         toml_values.extend(new_values)
         sys.stderr.write("\n")
         return new_values
+
+    def clear(self) -> None:
+        sys.stdout.write("\033[2J\033[H") if os.name != "nt" else sys.stdout.write(
+            "\033[2J"
+        )
+        sys.stdout.flush()

variantlib/commands/config/setup_interfaces/urwid_ui.py
@@ -103,7 +103,7 @@ class UrwidUI:
             return False
         return True
 
-    def input_bool(self, prompt: str, default: bool) -> bool:
+    def input_bool(self, prompt: str, default: bool, height: int = 6) -> bool:
         class State:
             retval: bool
 
@@ -158,7 +158,7 @@ class UrwidUI:
                 align=urwid.CENTER,
                 width=(urwid.RELATIVE, 50),
             ),
-            height=6,
+            height=height,
         )
         loop = urwid.MainLoop(frame, self.palette, unhandled_input=State.input_handler)
         loop.run()
@@ -276,3 +276,7 @@ class UrwidUI:
         toml_values.clear()
         toml_values.extend(new_values)
         return new_values
+
+    def clear(self) -> None:
+        # No-op for urwid UI
+        pass

commit 3b9ff1d5ae36b8b68acb8a4ce2cd14e32048cc07

Fixes to regressions in 2197d01b0c0600494b97b7efdc47193a5acf7aa3 (#71)
* Fix missing type hints

* Remove non-existing `clear_console` kwarg

* Fixes

---------

Co-authored-by: Jonathan Dekhtiar <jonathan@dekhtiar.com>

variantlib/commands/config/setup.py
@@ -229,9 +229,7 @@ def setup(args: list[str]) -> None:
         )
         if parsed_args.ui != "urwid":
             if not ui.input_bool(
-                "Do you want to save the configuration changes?",
-                default=True,
-                clear_console=False,
+                "Do you want to save the configuration changes?", default=True
             ):
                 sys.stdout.write("Configuration changes discarded\n")
                 return

variantlib/commands/config/setup_interfaces/console_ui.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from types import ModuleType
+    from typing import Any
 
     from tomlkit.toml_document import TOMLDocument
 
@@ -56,7 +57,7 @@ class ConsoleUI:
             return False
         return True
 
-    def input_bool(self, prompt: str, default: bool, *args, **kwargs) -> bool:
+    def input_bool(self, prompt: str, default: bool, *args: Any, **kwargs: Any) -> bool:
         full_prompt = f"{prompt} [{'Y/n' if default else 'y/N'}] "
         while True:
             val = input(full_prompt)