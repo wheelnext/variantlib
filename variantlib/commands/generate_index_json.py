763a7ec6277ca3ffd369e3157e4c611753197ba6
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-05-02 13:05:20 -0400

    Review Fixes

--- variantlib/commands/generate_index_json.py
+++ variantlib/commands/generate_index_json.py
@@ -3,35 +3,14 @@ from __future__ import annotations
 import argparse
 import email.parser
 import email.policy
-import json
 import logging
 import pathlib
 import zipfile
-from collections import defaultdict
 
 from variantlib import __package_name__
-from variantlib.constants import METADATA_VARIANT_DEFAULT_PRIO_FEATURE_HEADER
-from variantlib.constants import METADATA_VARIANT_DEFAULT_PRIO_NAMESPACE_HEADER
-from variantlib.constants import METADATA_VARIANT_DEFAULT_PRIO_PROPERTY_HEADER
-from variantlib.constants import METADATA_VARIANT_PROPERTY_HEADER
-from variantlib.constants import METADATA_VARIANT_PROVIDER_ENTRYPOINT_HEADER
-from variantlib.constants import METADATA_VARIANT_PROVIDER_REQUIRES_HEADER
-from variantlib.constants import VALIDATION_NAMESPACE_REGEX
-from variantlib.constants import VALIDATION_PROVIDER_ENTRYPOINT_REGEX
-from variantlib.constants import VALIDATION_PROVIDER_REQUIRES_REGEX
+from variantlib.commands.index_json_utils import append_variant_info_to_json_file
 from variantlib.constants import VALIDATION_WHEEL_NAME_REGEX
-from variantlib.constants import VARIANTS_JSON_DEFAULT_PRIO_FEATURE_KEY
-from variantlib.constants import VARIANTS_JSON_DEFAULT_PRIO_NAMESPACE_KEY
-from variantlib.constants import VARIANTS_JSON_DEFAULT_PRIO_PROPERTY_KEY
-from variantlib.constants import VARIANTS_JSON_PROVIDER_DATA_KEY
-from variantlib.constants import VARIANTS_JSON_PROVIDER_ENTRY_POINT_KEY
-from variantlib.constants import VARIANTS_JSON_PROVIDER_REQUIRES_KEY
-from variantlib.constants import VARIANTS_JSON_VARIANT_DATA_KEY
 from variantlib.errors import ValidationError
-from variantlib.models.variant import VariantDescription
-from variantlib.models.variant import VariantProperty
-from variantlib.validators import validate_matches_re
-from variantlib.validators import validate_requirement_str
 
 logger = logging.getLogger(__name__)
 
@@ -60,6 +39,8 @@ def generate_index_json(args: list[str]) -> None:
 
     vprop_parser = email.parser.BytesParser(policy=email.policy.compat32)
 
+    seen_versions = set()
+
     for wheel in directory.glob("*.whl"):
         # Skip non wheel variants
         if (wheel_info := VALIDATION_WHEEL_NAME_REGEX.fullmatch(wheel.name)) is None:
@@ -75,6 +56,20 @@ def generate_index_json(args: list[str]) -> None:
                 {"input_file": wheel.name},
             )
             continue
+
+        if (pkg_version := wheel_info.group("ver")) is None:
+            logger.error(
+                "Filepath: `%(input_file)s` ... does not have a valid version. "
+                "Skipping ...",
+                {"input_file": wheel.name},
+            )
+            continue
+
+        if pkg_version not in seen_versions:
+            # Clean old JSON file if it exists at first encounter
+            # This is to avoid appending to an existing file
+            (directory / f"variants-{pkg_version}.json").unlink(missing_ok=True)
+            seen_versions.add(pkg_version)
 
         logger.info(
             "Processing wheel: `%(wheel)s` with variant hash: `%(vhash)s`",
@@ -93,243 +88,15 @@ def generate_index_json(args: list[str]) -> None:
                 logger.warning("%s: no METADATA file found", wheel)
                 continue
 
-            data = {}
-            modified = False
-            pkg_version = wheel_metadata.get("Version")
-
-            # ========== Loading existing file and setting default values ========== #
-
-            if (variant_fp := directory / f"variants-{pkg_version}.json").exists():
-                data = json.loads(variant_fp.read_text())
-
-            for key, default_val in [  # type: ignore[var-annotated]
-                (VARIANTS_JSON_VARIANT_DATA_KEY, {}),
-                (
-                    VARIANTS_JSON_PROVIDER_DATA_KEY,
-                    defaultdict(
-                        lambda: {
-                            VARIANTS_JSON_PROVIDER_REQUIRES_KEY: [],
-                            VARIANTS_JSON_PROVIDER_ENTRY_POINT_KEY: "",
-                        }
-                    ),
-                ),
-                (VARIANTS_JSON_DEFAULT_PRIO_NAMESPACE_KEY, []),
-                (VARIANTS_JSON_DEFAULT_PRIO_FEATURE_KEY, []),
-                (VARIANTS_JSON_DEFAULT_PRIO_PROPERTY_KEY, []),
-            ]:
-                data.setdefault(key, default_val)
-
-            # =================== Variant Properties Processing ==================== #
-
-            variant_properties = wheel_metadata.get_all(
-                METADATA_VARIANT_PROPERTY_HEADER, []
-            )
-
             try:
-                vprops = [
-                    VariantProperty.from_str(vprop) for vprop in variant_properties
-                ]
-                vdesc = VariantDescription(vprops)
+                append_variant_info_to_json_file(
+                    base_dir=directory,
+                    metadata=wheel_metadata,
+                )
             except ValidationError:
                 logger.exception(
-                    "%(wheel)s has been rejected due to invalid properties. Will be "
-                    "ignored.",
-                    {"wheel": wheel},
+                    "Failed to process wheel: `%(wheel)s` with variant hash: "
+                    "`%(vhash)s`",
+                    {"wheel": wheel.name, "vhash": vhash},
                 )
                 continue
-
-            if (vhash := vdesc.hexdigest) not in data[VARIANTS_JSON_VARIANT_DATA_KEY]:
-                modified = True
-                data[VARIANTS_JSON_VARIANT_DATA_KEY][vhash] = vdesc.to_dict()
-
-            # ===================== Variant Provider Requires ===================== #
-
-            error = False
-            for provider_req in wheel_metadata.get_all(
-                METADATA_VARIANT_PROVIDER_REQUIRES_HEADER, []
-            ):
-                if not (
-                    match := VALIDATION_PROVIDER_REQUIRES_REGEX.fullmatch(provider_req)
-                ):
-                    logger.error(
-                        "%(wheel)s has an invalid `%(key)s` value: `%(value)s`. "
-                        "Expected format: `<namespace>: <requirement_str>`",
-                        {
-                            "wheel": wheel,
-                            "key": METADATA_VARIANT_PROVIDER_REQUIRES_HEADER,
-                            "value": provider_req,
-                        },
-                    )
-                    error = True
-                    break
-
-                namespace = match.group("namespace").strip()
-                req_str = match.group("requirement_str").strip()
-
-                try:
-                    validate_matches_re(namespace, VALIDATION_NAMESPACE_REGEX)
-                    validate_requirement_str(req_str)
-                except ValidationError:
-                    logger.exception(
-                        "%(wheel)s has an invalid `%(key)s` value: `%(value)s`. "
-                        "Expected format: `<namespace>: <requirement_str>`",
-                        {
-                            "wheel": wheel,
-                            "key": METADATA_VARIANT_PROVIDER_REQUIRES_HEADER,
-                            "value": provider_req,
-                        },
-                    )
-                    error = True
-                    break
-
-                provider_data = data[VARIANTS_JSON_PROVIDER_DATA_KEY][namespace]
-                if req_str not in provider_data[VARIANTS_JSON_PROVIDER_REQUIRES_KEY]:
-                    modified = True
-                    provider_data[VARIANTS_JSON_PROVIDER_REQUIRES_KEY].append(req_str)
-
-            if error:
-                continue
-
-            # Validation:
-            # - Every default namespace has to be declared in the providers dictionary
-            # - Each provider has to at least have one "requires" entry
-            for namespace in data[VARIANTS_JSON_DEFAULT_PRIO_NAMESPACE_KEY]:
-                if (
-                    pdata := data[VARIANTS_JSON_PROVIDER_DATA_KEY].get(namespace)
-                ) is None or len(pdata[VARIANTS_JSON_PROVIDER_REQUIRES_KEY]) == 0:
-                    logger.error(
-                        "%(wheel)s has an invalid configuration. The variant namespace "
-                        "`%(namespace)s` does not provide any installation "
-                        "requirements. Expected format: `<namespace>: <requirement>`",
-                        {"wheel": wheel, "namespace": namespace},
-                    )
-                    error = True
-                    break
-
-            # ===================== Variant Provider Entry-Point ===================== #
-
-            error = False
-            for provider_entrypoint in wheel_metadata.get_all(
-                METADATA_VARIANT_PROVIDER_ENTRYPOINT_HEADER, []
-            ):
-                if not (
-                    match := VALIDATION_PROVIDER_ENTRYPOINT_REGEX.fullmatch(
-                        provider_entrypoint
-                    )
-                ):
-                    logger.error(
-                        "%(wheel)s has an invalid `%(key)s` value: `%(value)s`. "
-                        "Expected format: `<namespace>: <entry-point>`",
-                        {
-                            "wheel": wheel,
-                            "key": METADATA_VARIANT_PROVIDER_ENTRYPOINT_HEADER,
-                            "value": provider_entrypoint,
-                        },
-                    )
-                    error = True
-                    break
-
-                namespace = match.group("namespace").strip()
-                entrypoint_str = match.group("entrypoint").strip()
-
-                try:
-                    validate_matches_re(namespace, VALIDATION_NAMESPACE_REGEX)
-                except ValidationError:
-                    logger.exception(
-                        "%(wheel)s has an invalid `%(key)s` value: `%(value)s`. "
-                        "Expected format: `<namespace>: <entry-point>`",
-                        {
-                            "wheel": wheel,
-                            "key": METADATA_VARIANT_PROVIDER_ENTRYPOINT_HEADER,
-                            "value": provider_entrypoint,
-                        },
-                    )
-                    error = True
-                    break
-
-                provider_data = data[VARIANTS_JSON_PROVIDER_DATA_KEY][namespace]
-                if curr_val := provider_data[VARIANTS_JSON_PROVIDER_ENTRY_POINT_KEY]:
-                    if curr_val != entrypoint_str:
-                        logger.error(
-                            (
-                                "The entry-point for the variant namespace `%(ns)s` in "
-                                "the wheel `%(wheel)s` is not consistent. "
-                                "Expected: `%(expected)s`, Found: `%(found)s`"
-                            ),
-                            {
-                                "ns": namespace,
-                                "wheel": wheel,
-                                "expected": curr_val,
-                                "found": entrypoint_str,
-                            },
-                        )
-                        error = True
-                        break
-                    continue
-
-                provider_data[VARIANTS_JSON_PROVIDER_ENTRY_POINT_KEY] = entrypoint_str
-                modified = True
-
-            if error:
-                continue
-
-            # Validation:
-            # - Every default namespace has to be declared in the providers dictionary
-            # - Each provider has to at least have one "requires" entry
-            for namespace in data[VARIANTS_JSON_DEFAULT_PRIO_NAMESPACE_KEY]:
-                if (
-                    pdata := data[VARIANTS_JSON_PROVIDER_DATA_KEY].get(namespace)
-                ) is None or len(pdata[VARIANTS_JSON_PROVIDER_REQUIRES_KEY]) == 0:
-                    logger.error(
-                        "%(wheel)s has an invalid configuration. The variant namespace "
-                        "`%(namespace)s` does not provide any installation "
-                        "requirements. Expected format: `<namespace>: <requirement>`",
-                        {"wheel": wheel, "namespace": namespace},
-                    )
-                    error = True
-                    break
-
-            # ===================== Variant Default Priorities ===================== #
-
-            for source_key, target_key in [
-                (
-                    METADATA_VARIANT_DEFAULT_PRIO_NAMESPACE_HEADER,
-                    VARIANTS_JSON_DEFAULT_PRIO_NAMESPACE_KEY,
-                ),
-                (
-                    METADATA_VARIANT_DEFAULT_PRIO_FEATURE_HEADER,
-                    VARIANTS_JSON_DEFAULT_PRIO_FEATURE_KEY,
-                ),
-                (
-                    METADATA_VARIANT_DEFAULT_PRIO_PROPERTY_HEADER,
-                    VARIANTS_JSON_DEFAULT_PRIO_PROPERTY_KEY,
-                ),
-            ]:
-                if _value := wheel_metadata.get(source_key):
-                    value = [v.strip() for v in _value.split(",")]
-                else:
-                    value = []
-
-                if not data[target_key]:
-                    modified = True
-                    data[target_key] = value
-
-                elif data[target_key] != value:
-                    logger.error(
-                        (
-                            "`%(key)s` in the wheel `%(wheel)s` is not consistent. "
-                            "Expected: `%(expected)s`, Found: `%(found)s`"
-                        ),
-                        {
-                            "key": source_key,
-                            "wheel": wheel,
-                            "expected": data[target_key],
-                            "found": value,
-                        },
-                    )
-
-            # ====================== Write to Disk if modified ===================== #
-
-            if modified:
-                with variant_fp.open(mode="w") as f:
-                    json.dump(data, f, indent=4, sort_keys=True)
---
+++ variantlib/commands/index_json_utils.py
@@ -0,0 +1,225 @@
+from __future__ import annotations
+
+import json
+import logging
+from collections import defaultdict
+from typing import TYPE_CHECKING
+
+from variantlib.constants import METADATA_VARIANT_DEFAULT_PRIO_FEATURE_HEADER
+from variantlib.constants import METADATA_VARIANT_DEFAULT_PRIO_NAMESPACE_HEADER
+from variantlib.constants import METADATA_VARIANT_DEFAULT_PRIO_PROPERTY_HEADER
+from variantlib.constants import METADATA_VARIANT_PROPERTY_HEADER
+from variantlib.constants import METADATA_VARIANT_PROVIDER_ENTRYPOINT_HEADER
+from variantlib.constants import METADATA_VARIANT_PROVIDER_REQUIRES_HEADER
+from variantlib.constants import VALIDATION_METADATA_PROVIDER_ENTRY_POINT_REGEX
+from variantlib.constants import VALIDATION_METADATA_PROVIDER_REQUIRES_REGEX
+from variantlib.constants import VALIDATION_NAMESPACE_REGEX
+from variantlib.constants import VARIANTS_JSON_DEFAULT_PRIO_KEY
+from variantlib.constants import VARIANTS_JSON_FEATURE_KEY
+from variantlib.constants import VARIANTS_JSON_NAMESPACE_KEY
+from variantlib.constants import VARIANTS_JSON_PROPERTY_KEY
+from variantlib.constants import VARIANTS_JSON_PROVIDER_DATA_KEY
+from variantlib.constants import VARIANTS_JSON_PROVIDER_ENTRY_POINT_KEY
+from variantlib.constants import VARIANTS_JSON_PROVIDER_REQUIRES_KEY
+from variantlib.constants import VARIANTS_JSON_VARIANT_DATA_KEY
+from variantlib.errors import ValidationError
+from variantlib.models.variant import VariantDescription
+from variantlib.models.variant import VariantProperty
+from variantlib.validators import validate_matches_re
+from variantlib.validators import validate_requirement_str
+
+if TYPE_CHECKING:
+    import pathlib
+    from email.message import Message
+
+logger = logging.getLogger(__name__)
+
+
+def append_variant_info_to_json_file(
+    base_dir: pathlib.Path,
+    metadata: Message,
+) -> None:
+    """
+    Append variant information to a JSON file.
+
+    Args:
+        json_file (pathlib.Path): The path to the JSON file.
+        variant (VariantDescription): The variant description object.
+        wheel_name (str): The name of the wheel.
+
+    Raises:
+        ValidationError: If the JSON file is not valid or if the variant is not valid.
+    """
+    data = {}
+    modified = False
+    pkg_version = metadata.get("Version")
+
+    if pkg_version is None:
+        raise ValidationError("No version found in the `METADATA`")
+
+    # ========== Loading existing file and setting default values ========== #
+
+    if (variant_fp := base_dir / f"variants-{pkg_version}.json").exists():
+        data = json.loads(variant_fp.read_text())
+
+    for key, default_val in [  # type: ignore[var-annotated]
+        (VARIANTS_JSON_VARIANT_DATA_KEY, {}),
+        (
+            VARIANTS_JSON_PROVIDER_DATA_KEY,
+            defaultdict(
+                lambda: {
+                    VARIANTS_JSON_PROVIDER_REQUIRES_KEY: [],
+                    VARIANTS_JSON_PROVIDER_ENTRY_POINT_KEY: "",
+                }
+            ),
+        ),
+        (
+            VARIANTS_JSON_DEFAULT_PRIO_KEY,
+            {
+                VARIANTS_JSON_NAMESPACE_KEY: [],
+                VARIANTS_JSON_FEATURE_KEY: [],
+                VARIANTS_JSON_PROPERTY_KEY: [],
+            },
+        ),
+    ]:
+        data.setdefault(key, default_val)
+
+    # =================== Variant Properties Processing ==================== #
+
+    variant_properties = metadata.get_all(METADATA_VARIANT_PROPERTY_HEADER, [])
+
+    try:
+        vprops = [VariantProperty.from_str(vprop) for vprop in variant_properties]
+        vdesc = VariantDescription(vprops)
+    except ValidationError as e:
+        raise ValidationError("Invalid properties found") from e
+
+    if (vhash := vdesc.hexdigest) not in data[VARIANTS_JSON_VARIANT_DATA_KEY]:
+        modified = True
+        data[VARIANTS_JSON_VARIANT_DATA_KEY][vhash] = vdesc.to_dict()
+
+    # ===================== Variant Default Priorities ===================== #
+
+    pdata = data[VARIANTS_JSON_DEFAULT_PRIO_KEY]
+    for source_key, target_key in [
+        (
+            METADATA_VARIANT_DEFAULT_PRIO_NAMESPACE_HEADER,
+            VARIANTS_JSON_NAMESPACE_KEY,
+        ),
+        (
+            METADATA_VARIANT_DEFAULT_PRIO_FEATURE_HEADER,
+            VARIANTS_JSON_FEATURE_KEY,
+        ),
+        (
+            METADATA_VARIANT_DEFAULT_PRIO_PROPERTY_HEADER,
+            VARIANTS_JSON_PROPERTY_KEY,
+        ),
+    ]:
+        if _value := metadata.get(source_key):
+            value = [v.strip() for v in _value.split(",")]
+        else:
+            value = []
+
+        if not pdata[target_key]:
+            modified = True
+            pdata[target_key] = value
+
+        elif pdata[target_key] != value:
+            raise ValidationError(
+                f"Invalid configuration found. `{source_key}` is not consistent. "
+                f"Expected: `{data[target_key]}`, Found: `{value}`"
+            )
+
+    # ===================== Variant Provider Requires ===================== #
+
+    for provider_req in metadata.get_all(METADATA_VARIANT_PROVIDER_REQUIRES_HEADER, []):
+        try:
+            match = VALIDATION_METADATA_PROVIDER_REQUIRES_REGEX.fullmatch(provider_req)
+            if match is None:
+                raise ValidationError("Regex failure")  # noqa: TRY301
+
+            namespace = match.group("namespace").strip()
+            req_str = match.group("requirement_str").strip()
+            validate_matches_re(namespace, VALIDATION_NAMESPACE_REGEX)
+            validate_requirement_str(req_str)
+
+        except (ValidationError, IndexError):
+            raise ValidationError(
+                f"Invalid `{METADATA_VARIANT_PROVIDER_REQUIRES_HEADER}` value found: "
+                f"`{provider_req}`. Expected format: `<namespace>: <requirement_str>`"
+            ) from None
+
+        provider_data = data[VARIANTS_JSON_PROVIDER_DATA_KEY][namespace]
+        if req_str not in provider_data[VARIANTS_JSON_PROVIDER_REQUIRES_KEY]:
+            modified = True
+            provider_data[VARIANTS_JSON_PROVIDER_REQUIRES_KEY].append(req_str)
+
+    # Validation:
+    # - Every default namespace has to be declared in the providers dictionary
+    # - Each provider has to at least have one "requires" entry
+    for namespace in data[VARIANTS_JSON_DEFAULT_PRIO_KEY][VARIANTS_JSON_NAMESPACE_KEY]:
+        if (
+            pdata := data[VARIANTS_JSON_PROVIDER_DATA_KEY].get(namespace)
+        ) is None or len(pdata[VARIANTS_JSON_PROVIDER_REQUIRES_KEY]) == 0:
+            raise ValidationError(
+                "Invalid configuration found. The variant namespace "
+                f"`{namespace}` does not provide any installation "
+                "requirements. Expected format: `<namespace>: <requirement>`"
+            )
+
+    # ===================== Variant Provider Entry-Point ===================== #
+
+    for provider_entrypoint in metadata.get_all(
+        METADATA_VARIANT_PROVIDER_ENTRYPOINT_HEADER, []
+    ):
+        try:
+            match = VALIDATION_METADATA_PROVIDER_ENTRY_POINT_REGEX.fullmatch(
+                provider_entrypoint
+            )
+            if match is None:
+                raise ValidationError("Regex failure")  # noqa: TRY301
+
+            namespace = match.group("namespace").strip()
+            entrypoint_str = match.group("entrypoint").strip()
+
+            # no need to validate namespace or entrypoint_str again
+            # as they are already validated in the regex:
+            # `VALIDATION_METADATA_PROVIDER_ENTRY_POINT_REGEX`
+
+        except (ValidationError, IndexError):
+            raise ValidationError(
+                f"Invalid `{VALIDATION_METADATA_PROVIDER_ENTRY_POINT_REGEX}` value "
+                "found: `{provider_entrypoint}`. Expected format: "
+                "`<namespace>: <entry-point>`"
+            ) from None
+
+        provider_data = data[VARIANTS_JSON_PROVIDER_DATA_KEY][namespace]
+        if curr_val := provider_data[VARIANTS_JSON_PROVIDER_ENTRY_POINT_KEY]:
+            if curr_val != entrypoint_str:
+                raise ValidationError(
+                    "Invalid configuration found. The entry-point for the variant "
+                    f"namespace `{namespace}` is not consistent. "
+                    f"Expected: `{curr_val}`, Found: `{entrypoint_str}`"
+                )
+            continue
+
+        modified = True
+        provider_data[VARIANTS_JSON_PROVIDER_ENTRY_POINT_KEY] = entrypoint_str
+
+    # Validation:
+    # - Every default namespace has to be declared in the providers dictionary
+    # - Each provider has to at least have one "requires" entry
+    for namespace in data[VARIANTS_JSON_DEFAULT_PRIO_KEY][VARIANTS_JSON_NAMESPACE_KEY]:
+        if (
+            pdata := data[VARIANTS_JSON_PROVIDER_DATA_KEY].get(namespace)
+        ) is None or len(pdata[VARIANTS_JSON_PROVIDER_REQUIRES_KEY]) == 0:
+            raise ValidationError(
+                f"Invalid configuration found. The variant namespace `{namespace}` "
+                "does not provide any installation requirements. Expected format: "
+                "`<namespace>: <requirement>`"
+            )
+
+    # ====================== Write to Disk if modified ===================== #
+    if modified:
+        with variant_fp.open(mode="w") as f:
+            json.dump(data, f, indent=4, sort_keys=True)
