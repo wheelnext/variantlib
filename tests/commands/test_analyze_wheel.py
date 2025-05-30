7ca3fd9aee75e5b2fd0bd8372bb32210bdced957
Michał Górny <mgorny@quansight.com>
2025-05-30 16:37:06 +0200

    Add initial tests for `analyze-wheel` command

---
+++ tests/commands/test_analyze_wheel.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from variantlib.commands.main import main
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    import pytest
+
+
+def test_analyze_wheel_regular(
+    caplog: pytest.LogCaptureFixture,
+    capsys: pytest.CaptureFixture[str],
+    mocked_entry_points: None,
+    test_plugin_package_wheel_path: Path,
+) -> None:
+    main(["analyze-wheel", "-i", str(test_plugin_package_wheel_path)])
+    assert capsys.readouterr().out == ""
+    assert "Standard Wheel" in caplog.text
+
+
+def test_analyze_wheel_null_variant(
+    capsys: pytest.CaptureFixture[str],
+    mocked_entry_points: None,
+    test_plugin_package_wheel_path: Path,
+) -> None:
+    main(
+        [
+            "analyze-wheel",
+            "-i",
+            "tests/artifacts/test_plugin_package-0-py3-none-any-00000000.whl",
+        ]
+    )
+    assert (
+        capsys.readouterr().out
+        == """\
+############################## Variant: `00000000` #############################
+################################################################################
+"""
+    )
