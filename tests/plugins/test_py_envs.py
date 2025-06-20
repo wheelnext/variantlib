e012cf8d643681a991318f0ba4b958d106705877
Michał Górny <mgorny@quansight.com>
2025-06-20 18:33:12 +0200

    Remove support for installing and creating venv entirely

--- tests/plugins/test_py_envs.py
+++ tests/plugins/test_py_envs.py
@@ -6,35 +6,22 @@ import sysconfig
 import venv
 from pathlib import Path
 
-import pytest
-
 from variantlib.plugins.py_envs import python_env
 
 
 def test_system_env() -> None:
-    with python_env(isolated=False) as env:
+    with python_env() as env:
         assert env.venv_path is None
         assert env.python_executable == Path(sys.executable)
 
 
-@pytest.mark.parametrize("custom", [False, True])
-def test_isolated_env(
-    custom: bool, tmp_path: Path, test_plugin_package_req: str
-) -> None:
-    if custom:
-        venv.create(tmp_path, with_pip=True)
-    with python_env(isolated=True, venv_path=tmp_path if custom else None) as env:
-        assert env.venv_path is not None
-        if custom:
-            assert env.venv_path == tmp_path
+def test_isolated_env(tmp_path: Path, test_plugin_package_req: str) -> None:
+    venv.create(tmp_path, with_pip=True)
+    with python_env(venv_path=tmp_path) as env:
+        assert env.venv_path == tmp_path
         script_dir = Path(
             sysconfig.get_path("scripts", vars={"base": str(env.venv_path)})
         )
         assert env.python_executable == script_dir / (
             "python.exe" if os.name == "nt" else "python"
         )
-
-        env.install([test_plugin_package_req])
-
-        purelib = Path(sysconfig.get_path("purelib", vars={"base": str(env.venv_path)}))
-        assert (purelib / "test_plugin_package.py").exists()
--- variantlib/plugins/py_envs.py
+++ variantlib/plugins/py_envs.py
@@ -1,30 +1,18 @@
 from __future__ import annotations
 
-import functools
 import logging
 import os
 import pathlib
 import sys
 import sysconfig
-import tempfile
-import typing
 from contextlib import contextmanager
 from typing import TYPE_CHECKING
 
-from variantlib.plugins.py_backends import AutoInstallBackend
-from variantlib.plugins.py_backends import PipBackend
-from variantlib.plugins.py_backends import UvBackend
-
 if TYPE_CHECKING:
-    from collections.abc import Collection
     from collections.abc import Generator
 
 
 logger = logging.getLogger(__name__)
-
-Installer = typing.Literal["pip", "uv"]
-
-INSTALLERS = typing.get_args(Installer)
 
 
 def _find_executable(path: pathlib.Path) -> pathlib.Path:
@@ -78,51 +66,9 @@ def _find_executable(path: pathlib.Path) -> pathlib.Path:
     return executable
 
 
-@functools.cache
-def _fs_supports_symlink() -> bool:
-    """Return True if symlinks are supported"""
-    # Using definition used by venv.main()
-    if os.name != "nt":
-        return True
-
-    # Windows may support symlinks (setting in Windows 10)
-    with tempfile.NamedTemporaryFile(prefix="variant-symlink-") as tmp_file:
-        dest = f"{tmp_file}-b"
-        try:
-            os.symlink(tmp_file.name, dest)
-            os.unlink(dest)  # noqa: PTH108
-        except (OSError, NotImplementedError, AttributeError):
-            return False
-        return True
-
-
 class PythonEnv:
-    _env_backend: UvBackend | PipBackend = AutoInstallBackend()
-
     def __init__(self, venv_path: pathlib.Path | None = None):
         self.venv_path = venv_path
-
-    def install(self, requirements: Collection[str]) -> None:
-        """
-        Install packages from PEP 508 requirements in the isolated variant plugin
-        environment.
-
-        :param requirements: PEP 508 requirement specification to install
-
-        :note: Passing non-PEP 508 strings will result in undefined behavior, you
-               *should not* rely on it. It is merely an implementation detail, it may
-               change any time without warning.
-        """
-        if not requirements:
-            return
-
-        logger.info(
-            "Installing packages in current environment:\n%(reqs)s",
-            {"reqs": "\n".join(f"- {r}" for r in sorted(requirements))},
-        )
-        self._env_backend.install_requirements(
-            requirements, py_exec=self.python_executable
-        )
 
     @property
     def python_executable(self) -> pathlib.Path:
@@ -134,23 +80,5 @@ class PythonEnv:
 
 
 @contextmanager
-def python_env(
-    isolated: bool = False, venv_path: pathlib.Path | None = None
-) -> Generator[PythonEnv]:
-    if venv_path is None and isolated:
-        import venv
-
-        with tempfile.TemporaryDirectory(prefix="variantlib-venv") as temp_dir:
-            logger.info(
-                "Creating virtual environment in %(path)s ...",
-                {"path": str(temp_dir)},
-            )
-            venv.EnvBuilder(
-                symlinks=_fs_supports_symlink(),
-                with_pip=True,
-                system_site_packages=False,
-                clear=True,
-            ).create(temp_dir)
-            yield PythonEnv(pathlib.Path(temp_dir))
-    else:
-        yield PythonEnv(venv_path)
+def python_env(venv_path: pathlib.Path | None = None) -> Generator[PythonEnv]:
+    yield PythonEnv(venv_path)
