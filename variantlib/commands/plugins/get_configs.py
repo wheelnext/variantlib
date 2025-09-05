94934629819431fd96dd36f0c095c3d8ed9ea2e7
Michał Górny <mgorny@quansight.com>
2025-09-05 15:25:02 +0200

    Make `variantlib plugins get-configs` into a generic method
    Signed-off-by: Michał Górny <mgorny@quansight.com>

--- variantlib/commands/plugins/get_configs.py
+++ variantlib/commands/plugins/get_configs.py
@@ -14,18 +14,35 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-def get_supported_configs(args: list[str], plugin_loader: BasePluginLoader) -> None:
+def get_configs(args: list[str], plugin_loader: BasePluginLoader) -> None:
     parser = argparse.ArgumentParser(
-        prog=f"{__package_name__} plugins get-supported-configs",
-        description="CLI interface to get all supported configs on the machine",
+        prog=f"{__package_name__} plugins get-configs",
+        description="CLI interface to get configs",
     )
 
+    config_group = parser.add_mutually_exclusive_group(required=True)
+    config_group.add_argument(
+        "-a",
+        "--all",
+        action="store_const",
+        dest="method",
+        const="get_all_configs",
+        help="Get all valid configs",
+    )
+    config_group.add_argument(
+        "-s",
+        "--supported",
+        action="store_const",
+        dest="method",
+        const="get_supported_configs",
+        help="Get supported configs",
+    )
     parser.add_argument("-n", "--namespace", help="filter by namespace")
     parser.add_argument("-f", "--feature", help="filter by feature name")
 
     parsed_args = parser.parse_args(args)
 
-    for provider_cfg in plugin_loader.get_supported_configs().values():
+    for provider_cfg in getattr(plugin_loader, parsed_args.method)().values():
         if parsed_args.namespace not in (provider_cfg.namespace, None):
             continue
 
